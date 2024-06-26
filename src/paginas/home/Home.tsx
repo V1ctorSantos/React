515556256bfb81726b5e94f2802bc63260d060e8
V1ctorSantos <VictorSantos04@outlook.com.br>
2024-06-25 23:36:29 -0300

    introdução ao react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img className="img" src="https://garden.spoonflower.com/c/6940400/p/f/m/zQglCzzjKvrtu--5hX5c6Gt9dSe0dcaIh2FflwLq-0GUboqAbSw/Scattered%20Music%20Notes%20on%20Purple.jpg"
+            />
+        </>
+    );
+}
+
+export default Home;
+
+
